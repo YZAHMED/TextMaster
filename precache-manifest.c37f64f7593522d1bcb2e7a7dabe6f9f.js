0c76e5bb41da0388a66c7cab2d52ae884890b12c
YZAHMED <yaqoob.albalooshi@gmail.com>
2025-05-20 00:38:18 -0400

    Add required files for TaskMaster app

---
+++ precache-manifest.c37f64f7593522d1bcb2e7a7dabe6f9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d2e13841e142716ec79649966c731f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c523d79c3b97bbd53025",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "7607cb5e051352a3c801",
+    "url": "/static/js/2.e8b5e0c5.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/static/js/2.e8b5e0c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7427fe9bee336e9698de",
+    "url": "/static/js/3.4776b16b.chunk.js"
+  },
+  {
+    "revision": "c523d79c3b97bbd53025",
+    "url": "/static/js/main.f7c7d8fc.chunk.js"
+  },
+  {
+    "revision": "fc8e8a007f207401f191",
+    "url": "/static/js/runtime-main.6d0c0df4.js"
+  }
+]);
